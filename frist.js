fd337c26aadc98d9d39b14ec5d6a08122e17d3cf
Romil Sangani <ssanganiromil@gamil.com>
2025-02-14 19:08:15 +0530

    my pro

--- frist.js
+++ frist.js
@@ -88,17 +88,35 @@
 
 /***We are given array of marks of students. Filter our of the marks of students that scored 90+.***/
 // let marck=[85,78,98,96,45,78,92,97,75,65,52];
-// let finelres=marck.filter((val)=>{
+// let topper=marck.filter((val)=>{
 //     return val >= 90;
 // })
-// console.log(finelres);
- let n=prompt("Enter Number");
- let arr=[];
- for(i=1;i<=n;i++){
-    arr[i-1]=i;
- }
- console.log(arr);
- let sum=arr.reduce((prev,cru)=>{
-    return prev+cru;
- })
- console.log(`sum =${sum}`);
+// console.log(topper);
+
+
+/*Qs. Take a number n as input from user. Create an array of numbers from 1 to n.
+    Use the reduce method to calculate sum of all numbers in the array.
+    Use the reduce method to calculate product of all numbers in the array.
+*/
+
+
+//  let n=prompt("Enter Number");
+//  let arr=[];
+//  for(i=1;i<=n;i++){
+//     arr[i-1]=i;
+//  }
+//  console.log(arr);
+//  let sum=arr.reduce((prev,cru)=>{
+//     return prev+cru;
+//  })
+//  console.log(`sum =${sum}`);
+
+//  let factorial= arr.reduce((prev,cru)=>{
+//     return prev*cru;
+//  })
+//  console.log(factorial);
+console.dir(document.body);
+console.log(document.body);
+// document.body.style.backgroundColor="red";  
+// document.body.childNodes[1].innerText="abc";  
+document.getElementById("heading");
